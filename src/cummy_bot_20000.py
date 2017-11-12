721b6a80271099c486402c45986df02e9c26785d
William Harvey <williamnharvey@gmail.com>
2017-11-12 18:07:11 -0500

    logging in every minute is lame
    just run the cron every day or so and have it execute the script with a name (exec -a copypastabot cummy_bot_20000.py) after killing the previous one (kill copypastabot)

--- src/cummy_bot_20000.py
+++ src/cummy_bot_20000.py
@@ -1,42 +1,38 @@
-#* * * * * cd ~/python/CummyBot20000/src; python cummy_bot_20000.py
-
 import praw
 import datetime
+import time
 
-def login():
-    reddit = praw.Reddit('cummy_bot_20000')
-    return reddit
+r = praw.Reddit('cummy_bot_20000')
+copypasta = r.subreddit("copypasta")
+localSub = r.subreddit("copypasta")
 
-def run(r):
-    copypasta = r.subreddit("copypasta")
-    localSub = r.subreddit("redditscripttemplate")
 
-    #Checks the top 5 posts in the "new" category in the subreddit
+def run():
+    #Checks the top 10 posts in the "new" category in the subreddit
     num_posts_to_check = 5
-
     for submission in localSub.new(limit=num_posts_to_check):
         time_now = datetime.datetime.utcnow()
         submission_time = datetime.datetime.utcfromtimestamp(submission.created_utc)
-        time_since = (time_now - submission_time).total_seconds()
-        if time_since <= 60: #change according to frequency script is run
+        time_since = int((time_now - submission_time).total_seconds())
+        if time_since <= 10: #change according to frequency script is run
             title = submission.title
             body = submission.selftext
-            #url = submission.url
 
             #this can be made to almost always find a relevant response using a while loop
             #making the search query less specific every loop until something new is found
             #base case of no response would then occur when search query is most basic ie one word
             #not searching with the body because the query can be too big
-            responseText = ''
-            startTime = datetime.datetime.utcnow()
-            while(responseText == ''):
-                for result in copypasta.search(title):
-                    if responseText == '' and result.selftext != '' and result.selftext != body:
-                        responseText = result.selftext
-                if (datetime.datetime.utcnow() - startTime).total_seconds > 15:
+
+            for result in copypasta.search(title.encode("utf8"), 'relevance', 'plain', 'all'):
+                if result.selftext.encode("utf8") != '' and result.selftext.encode("utf8") != body.encode("utf8"):
+                    submission.reply(result.selftext.encode("utf8"))
                     break
 
-            if responseText:
-                submission.reply(responseText)
 
-run(login())
+def main():
+    while(1):
+        run()
+        time.sleep(10)
+
+if __name__ == '__main__':
+    main()
